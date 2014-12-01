314dbc511c6cb882c8bafafb07c4f4514ce34cc1
Mas Ben <Mas Ben@MasBen-PC>
2014-12-01 13:16:18 +0700

    03-09-2014-0852PM

--- balairungs-01122014.sql
+++ balairungs-01122014.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2014 at 03:40 AM
+-- Generation Time: Dec 01, 2014 at 07:15 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -23,15 +23,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mainmenu`
+--
+
+CREATE TABLE IF NOT EXISTS `mainmenu` (
+`id` int(3) NOT NULL,
+  `text_ind` varchar(20) NOT NULL,
+  `text_ing` varchar(20) NOT NULL,
+  `img` text NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `post`
 --
 
 CREATE TABLE IF NOT EXISTS `post` (
   `id` int(4) NOT NULL DEFAULT '0',
-  `Judul` varchar(100) NOT NULL,
+  `judul_indonesia` varchar(100) NOT NULL,
+  `judul_inggris` int(11) NOT NULL,
   `userId` int(4) NOT NULL,
-  `Isi` text NOT NULL
+  `isi_indonesia` text NOT NULL,
+  `isi_inggris` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submenu`
+--
+
+CREATE TABLE IF NOT EXISTS `submenu` (
+`id` int(11) NOT NULL,
+  `text_ind` text NOT NULL,
+  `text_ing` text NOT NULL,
+  `parent` varchar(3) NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -61,10 +91,22 @@ INSERT INTO `user` (`id`, `username`, `password`, `role`, `hape`, `email`) VALUE
 --
 
 --
+-- Indexes for table `mainmenu`
+--
+ALTER TABLE `mainmenu`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `submenu`
+--
+ALTER TABLE `submenu`
+ ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `user`
@@ -76,6 +118,16 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `mainmenu`
+--
+ALTER TABLE `mainmenu`
+MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `submenu`
+--
+ALTER TABLE `submenu`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
